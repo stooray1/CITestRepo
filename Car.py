f1aaed344cd31f28dd3aa2f1a0ca7597f0124d58
Tariq <mkha153@aucklanduni.ac.nz>
2021-06-10 13:12:28 +1200

    brake changed for testing

--- Car.py
+++ Car.py
@@ -12,8 +12,8 @@ class Car:
         self.speed += 5
 
     def brake(self):
-        #if self.speed > 0:
-        self.speed -= 5
+        if self.speed > 0:
+            self.speed -= 5
 
     def step(self):
         self.odometer += self.speed
